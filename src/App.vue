32fca50f6583130b6b82d5fc208768c21e5b3f5d
皮娟 <trytry8090@163.com>
2018-01-30 19:15:42 +0800

    no message

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,17 @@
 </script>
 
 <style>
+  html, body, ul, li {
+    margin: 0;
+    padding: 0;
+  }
+
+  ul, li {
+    list-style: none;
+  }
+
   #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    font-family: 'Microsoft YaHei', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
--- src/pages/nav/Nav.vue
+++ src/pages/nav/Nav.vue
@@ -1,12 +1,33 @@
 <template>
   <div class="nav">
-    
+    <ul>
+      <li>感受三生石尚</li>
+      <li>美妙瞬间</li>
+      <li>服务</li>
+      <li>优惠</li>
+    </ul>
+    <ul class=" ">
+      <li>微信</li>
+      <li>微博</li>
+      <li>留下您的信息</li>
+    </ul>
+    <ul>
+      <li>Add:</li>
+      <li>北京市朝阳区雅成一里甲三号</li>
+      <li>Tel:</li>
+      <li>4000 000</li>
+    </ul>
+    <a href="javascript:;">www.pragmalove4s.com</a>
   </div>
 </template>
 <style>
-  .nav{
-    width: 20vw;
+  .nav {
     height: 100vh;
     background-color: #fff;
+    position: absolute;
+    top: 0;
+    right: 0;
+    z-index: 100;
+    padding: 0 2vw;
   }
 </style>
