68cfce994abe0a5843981ba887f90921bd4a66e0
王跃 <hust_wangyue@foxmail.com>
2018-02-05 11:57:45 +0800

    提取 serviceItem mixin

--- src/pages/service/BottomService.vue
+++ src/pages/service/BottomService.vue
@@ -12,36 +12,11 @@
 </template>
 
 <script>
+  import { serviceItemMixin } from '../../mixins'
+
   export default {
     name: 'bottom-service',
-    props: ['imgUrl', 'gifUrl', 'color', 'title', 'content'],
-    data () {
-      return {
-        hover: false
-      }
-    },
-    computed: {
-      showUrl () {
-        return this.hover ? this.gifUrl : this.imgUrl
-      },
-      backgroundColor () {
-        return this.hover ? this.color : '#fff'
-      },
-      titleColor () {
-        return this.hover ? '#fff' : this.color
-      },
-      contentColor () {
-        return this.hover ? 'rgba(255,255,255,.9)' : '#a7a7a7'
-      }
-    },
-    methods: {
-      mouseenter () {
-        this.hover = true
-      },
-      mouseleave () {
-        this.hover = false
-      }
-    }
+    mixins: [serviceItemMixin]
   }
 </script>
 
