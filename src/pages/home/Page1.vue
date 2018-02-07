63760c432432a2cdc3595c61baeeef46cdcd68db
wangyue <hust_wangyue@foxmail.com>
2018-02-07 23:15:46 +0800

    no message

--- src/pages/home/Page1.vue
+++ src/pages/home/Page1.vue
@@ -1,25 +1,42 @@
 <template>
   <div class="homePage">
     <span class="colorfulLogo"></span>
-    <img class="banner1" src="../../assets/page1/banner1.png">
-    <img class="banner2" src="../../assets/page1/banner2.png">
-    <img class="banner3" src="../../assets/page1/banner3.png">
-    <img class="banner4" src="../../assets/page1/banner4.png">
-    <img class="banner5" src="../../assets/page1/banner5.png">
-    <img class="banner6" src="../../assets/page1/banner6.png">
-    <img class="banner7" src="../../assets/page1/banner7.png">
-    <img class="banner10" src="../../assets/page1/banner10.png">
-    <img class="banner8" src="../../assets/page1/banner8.png">
-    <div class="banner9Shadow"></div>
-    <img class="banner9" src="../../assets/page1/banner9.png">
-    <img class="banner11" src="../../assets/page1/banner11.png">
-    <img class="banner12" src="../../assets/page1/banner12.png">
+    <img ref="banner1" class="banner1" src="../../assets/page1/banner1.png">
+    <img ref="banner2" class="banner2" src="../../assets/page1/banner2.png">
+    <img ref="banner3" class="banner3" src="../../assets/page1/banner3.png">
+    <img ref="banner4" class="banner4" src="../../assets/page1/banner4.png">
+    <img ref="banner5" class="banner5" src="../../assets/page1/banner5.png">
+    <img ref="banner6" class="banner6" src="../../assets/page1/banner6.png">
+    <img ref="banner7" class="banner7" src="../../assets/page1/banner7.png">
+    <img ref="banner10" class="banner10" src="../../assets/page1/banner10.png">
+    <img ref="banner8" class="banner8" src="../../assets/page1/banner8.png">
+    <div ref="banner9Shadow" class="banner9Shadow"></div>
+    <img ref="banner9" class="banner9" src="../../assets/page1/banner9.png">
+    <img ref="banner11" class="banner11" src="../../assets/page1/banner11.png">
+    <img ref="banner12" class="banner12" src="../../assets/page1/banner12.png">
   </div>
 </template>
 
 <script>
+  import { TweenMax } from 'gsap'
+
   export default {
-    name: 'page1'
+    name: 'page1',
+    mounted () {
+      const {banner1, banner2, banner3, banner4, banner5, banner6, banner7, banner8, banner9, banner10, banner11, banner12} = this.$refs
+      TweenMax.to(banner1, 30, {rotationZ: 360, repeat: Infinity, ease: Linear.easeNone, repeatDelay: 0})
+      TweenMax.to(banner2, 30, {rotationZ: 360, repeat: Infinity, ease: Linear.easeNone, repeatDelay: 0})
+      TweenMax.to(banner3, 30, {rotationZ: 360, repeat: Infinity, ease: Linear.easeNone, repeatDelay: 0})
+      TweenMax.to(banner4, 30, {rotationZ: 360, repeat: Infinity, ease: Linear.easeNone, repeatDelay: 0})
+      TweenMax.to(banner5, 30, {rotationZ: -360, repeat: Infinity, ease: Linear.easeNone, repeatDelay: 0})
+      TweenMax.to(banner6, 30, {rotationZ: -360, repeat: Infinity, ease: Linear.easeNone, repeatDelay: 0})
+      TweenMax.to(banner7, 30, {rotationZ: -360, repeat: Infinity, ease: Linear.easeNone, repeatDelay: 0})
+      TweenMax.to(banner8, 30, {rotationZ: 360, repeat: Infinity, ease: Linear.easeNone, repeatDelay: 0})
+      TweenMax.to(banner9, 30, {rotationZ: -360, repeat: Infinity, ease: Linear.easeNone, repeatDelay: 0})
+      TweenMax.to(banner10, 30, {rotationZ: 360, repeat: Infinity, ease: Linear.easeNone, repeatDelay: 0})
+      TweenMax.to(banner11, 30, {rotationZ: -360, repeat: Infinity, ease: Linear.easeNone, repeatDelay: 0})
+      TweenMax.to(banner12, 30, {rotationZ: -360, repeat: Infinity, ease: Linear.easeNone, repeatDelay: 0})
+    }
   }
 </script>
 
@@ -32,48 +49,56 @@
     }
     .banner1 {
       width: 36.25vw;
-      top: 16vw;
+      top: 28.48vh;
       left: 13vw;
+      transform-origin: 37vw 21.52vh;
     }
     .banner2 {
       width: 54.875vw;
-      top: 20vw;
-      right: -7vw;
+      top: 39vh;
+      left: 55vw;
+      transform-origin: -5vw 11vh;
     }
     .banner3 {
       width: 33.75vw;
-      top: -4vw;
-      right: 13vw;
+      top: -7.12vh;
+      left: 53vw;
+      transform-origin: -3vw 57.12vh;
     }
     .banner4 {
       width: 15.54vw;
-      bottom: -14vw;
-      right: 33vw;
+      top: 50vh;
+      left: 52vw;
+      transform-origin: -2vw 0;
     }
     .banner5 {
       width: 15.83vw;
-      bottom: 0vw;
-      right: 28vw;
+      top: 74vh;
+      left: 57vw;
+      transform-origin: -7vw -24vh;
     }
     .banner6 {
       width: 8.96vw;
-      top: 17.3vw;
-      left: 21vw;
+      top: 30.79vh;
+      left: 19vw;
+      transform-origin: 31vw 19.21vh;
     }
     .banner7 {
       width: 12.04vw;
-      top: 7vw;
-      right: 26.5vw;
+      top: 12.46vh;
+      left: 63.5vw;
+      transform-origin: -13.5vw 37.54vh;
     }
     .banner8 {
       width: 20vw;
-      top: -8vw;
+      top: -14.24vh;
       left: 25vw;
+      transform-origin: 25vw 64.24vh;
     }
     .banner9Shadow {
       width: 15.71vw;
       height: 5vw;
-      top: -1vw;
+      top: -1.78vh;
       left: 25vw;
       border-radius: 50%;
       background-color: rgba(73, 41, 35, 0.25);
@@ -83,23 +108,27 @@
     }
     .banner9 {
       width: 18.71vw;
-      top: -10vw;
+      top: -17.8vh;
       left: 20vw;
+      transform-origin: 30vw 57.8vh;
     }
     .banner10 {
       width: 31vw;
-      bottom: -0.5vw;
+      top: 52vh;
       left: 4vw;
+      transform-origin: 46vw -2vh;
     }
     .banner11 {
       width: 19.29vw;
-      bottom: -1vw;
+      top: 78vh;
       left: 24vw;
+      transform-origin: 26vw -28vh;
     }
     .banner12 {
       width: 21.33vw;
-      top: -3vw;
-      right: 36vw;
+      top: -5.34vw;
+      left: 45vw;
+      transform-origin: 5vw 55.34vh;
     }
   }
 
