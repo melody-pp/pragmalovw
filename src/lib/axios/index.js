090285e3cb7e4f33e357108c044c404ee9909c47
wangyue <hust_wangyue@foxmail.com>
2018-02-07 21:51:13 +0800

    lang

--- src/lib/axios/index.js
+++ src/lib/axios/index.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
 import axios from 'axios'
 
+let lang
+const language = navigator.language
+if (language.includes('zh')) {
+  lang = 'cn'
+} else if (language === 'en') {
+  lang = 'en'
+} else {
+  lang = 'jp'
+}
 Vue.prototype.axios = axios.create({
   headers: {'Content-Type': 'application/json'},
-  baseURL: '/cn/index.php/Api'
+  baseURL: `/${lang}/index.php/Api`
 })
